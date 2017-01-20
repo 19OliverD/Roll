1f73558c9c76831b4f6a872d1e6244d18f07e2ef
Oliver <19oliverd@students.harker.org>
2017-01-20 08:11:09 -0800

    Update to a few actions
    Spin to Win!

--- RollMain.java
+++ RollMain.java
@@ -2,27 +2,29 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 public class RollMain {
 public static void main(String[] args) {
-	
+
 //Definitions
 JFrame frame = new JFrame("Roll!");
 JPanel panel = new JPanel();
 JButton d20 = new JButton("d20");
 JButton d6 = new JButton("d6");
 JButton d8 = new JButton("d8");
-	
+JLabel label = new JLabel("Spin to Win!");
+
 	//Frame Parameters
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setSize(250,100);
+	frame.setSize(250,125);
 	frame.add(panel);
 	
 	//Panel Parameters
 	panel.add(d20);
 	panel.add(d6);
 	panel.add(d8);
-	
+	panel.add(label);
 	//Button Parameters
 	d6.addActionListener(new Action2());
 	d20.addActionListener(new Action());
